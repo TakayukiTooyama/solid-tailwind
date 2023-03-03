cb6f8b20665240357dd74c1fa6f3f87464bf788e
TakayukiTooyama <t.tooyama107@gmail.com>
2023-03-03 15:53:15 +0900

    :tada: v1

---
+++ src/root.tsx
@@ -0,0 +1,38 @@
+import { CacheBoundary } from 'solid-cache';
+import { Suspense } from 'solid-js';
+import {
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+  Title,
+} from 'solid-start';
+import './root.css';
+
+export default function Root() {
+  return (
+    <Html lang="ja">
+      <Head>
+        <Title>SolidJS x TailwindCSS Template</Title>
+        <Meta charset="utf-8" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Body>
+        <Suspense>
+          <ErrorBoundary>
+            <CacheBoundary>
+              <Routes>
+                <FileRoutes />
+              </Routes>
+            </CacheBoundary>
+          </ErrorBoundary>
+        </Suspense>
+        <Scripts />
+      </Body>
+    </Html>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div class="text-5xl">Hello, SoildJS x TailWindCSS Template</div>;
+}
